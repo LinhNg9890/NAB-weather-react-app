6fff6ddfc5fa8e78ad349060e54b05b3d86e95f8
Linh Nguyen Manh <linh.nguyenmanh@zoi.de>
2022-10-30 16:54:25 +0700

    update API key

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -1,5 +1,5 @@
 //the API from openweathermap
-const API_KEY = "32a5bb7b9aa1126387e06acad817149e";
+const API_KEY = "138a5b5ee8d542e654bebeb3d015569e";
 const API_URL_CURRENT = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
 const API_URL_3HOURS = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`;
 
